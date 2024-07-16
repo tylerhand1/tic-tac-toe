937c2bed56546de973fe8cd3849ec9b855a5eeb4
tylerhand1 <tyha7462@colorado.edu>
2024-07-16 15:04:16 -0600

    Updates to cors options and added post route /api/tictactoeroom

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,31 +4,39 @@ import { createServer } from 'node:http';
 import cors from 'cors';
 import config from './utils/config';
 
-const allowedOrigins = 'http://localhost:3000';
+const allowedOrigins = ['http://localhost:4000'];
 const options: cors.CorsOptions = {
   origin: allowedOrigins,
+  methods: ["GET", "POST"],
 };
 
 const app: Express = express();
 app.use(cors(options));
 app.use(express.json());
 
-app.get('/', (_req: Request, res: Response) => {
-  res.send('hello');
-});
-
 const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:4000'
+    origin: 'http://localhost:4000',
+    methods: ["GET", "POST"],
   }
 });
 
-io.listen(config.CORS_PORT);
-
 io.on('connection', _socket => {
-  
+
 });
+
+app.get('/ping', (_req: Request, res: Response) => {
+  res.send('pong');
+});
+
+app.post('/api/tictactoeroom', (_req: Request, res: Response) => {
+  res.status(200).send({
+    "create": "success"
+  });
+});
+
+app.listen(config.CORS_PORT);
 
 export default server;
