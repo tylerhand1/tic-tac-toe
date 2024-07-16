b8d1b9c36251ba21cacdedeca326de5b53e76e43
tylerhand1 <tyha7462@colorado.edu>
2024-07-16 12:24:53 -0600

    Refactored server and app logic from index.ts to app.ts

---
+++ backend/src/app.ts
@@ -0,0 +1,33 @@
+import express, { Express, Request, Response } from 'express';
+import { Server } from 'socket.io';
+import { createServer } from 'node:http';
+import cors from 'cors';
+
+const allowedOrigins = ['http://localhost:4000'];
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+
+const app: Express = express();
+app.use(cors(options));
+app.use(express.json());
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send('hello');
+});
+
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:4000'
+  }
+});
+
+io.listen(4000);
+
+io.on('connection', _socket => {
+  
+});
+
+export default server;
