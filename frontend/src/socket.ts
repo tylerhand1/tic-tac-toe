451f2503f8d88219f919e68eb811a302e5a833c6
tylerhand1 <tyha7462@colorado.edu>
2024-07-17 18:54:15 -0600

    Changed IP address for socket

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from 'socket.io-client';
 
-const URL = 'http://localhost:3000';
+const URL = 'http://52.91.164.162:3000';
 
 export const socket: Socket = io(URL, {
   autoConnect: false,
