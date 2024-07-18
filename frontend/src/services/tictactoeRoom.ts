4ce570ee200631aeb32cba3dd22c63a5225022df
tylerhand1 <tyha7462@colorado.edu>
2024-07-17 18:51:50 -0600

    Changed IP address for tictactoeroom service

--- frontend/src/services/tictactoeRoom.ts
+++ frontend/src/services/tictactoeRoom.ts
@@ -4,7 +4,7 @@ import { CreateRoomResponse } from '@/types';
 axios.defaults.timeout = 5 * 1000;
 
 export const createRoom = async (): Promise<AxiosResponse<CreateRoomResponse>['data']> => {
-  const baseUrl = 'http://localhost:3000/api/create-room';
+  const baseUrl = 'http://52.91.164.162:3000/api/create-room';
   const response: AxiosResponse<CreateRoomResponse> = await axios.post(baseUrl);
   return response.data;
 };
